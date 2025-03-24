dce4383691122bee795b72322c9a1b8aa76a391f
livebcdev0603 <b.ceb.0603@gmail.com>
2025-03-24 08:41:05 +0000

    connected to postgresql server

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const { sequelize, testConnection } = require("./config/database");
+
+//Initialize express app
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+//Test database connection
+if(process.env.NODE_ENV !== 'test') {
+    testConnection();
+} 
+
+//404 handler
+app.use((req, res) => {
+    res.status(404).json({message: 'Route not found'});
+})
+
+//Erro handler
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    req.status(500).json({
+        message: "something went wrong",
+        error: process.env.NODE_ENV === "development" ? err.message : undefined
+    })
+})
+
+module.exports = app;
